9b1fe301ef95f96b72dd85a9e60cbad64a33c2a1
Nj3 <natarajitachi@hotmail.com>
2017-10-18 18:55:59 +0530

    updated old files removal functionality

--- SetWallpaper/BingWallpaper.py
+++ SetWallpaper/BingWallpaper.py
@@ -4,6 +4,7 @@ import os
 import datetime
 import platform
 import ctypes
+import time
 
 def set_wp(path):
     """After downloading the image from the site, it saves it in Picture
@@ -13,17 +14,34 @@ def set_wp(path):
     p = os.path.join(os.path.expanduser('~'),'Pictures')
     if not os.path.exists(p):
         os.mkdir(p)
-    urlretrieve(path, p+'/'+nam) # it saves the wallpaper in pictures folder
-    os.system('gsettings set org.gnome.desktop.background picture-uri file://'+'$HOME/Pictures/'+nam)
-
+    urlretrieve(path, os.path.join(p, nam)) # it saves the wallpaper in pictures folder
+    os.system('gsettings set org.gnome.desktop.background picture-uri file://'+ str(os.path.join(p, nam)))
+    clean_old(p)
 
 def set_bg(path):
     """After downloading the image from the site. save it in picture folder
      and sets the image as background. for windows"""
-    nam = os.path.join("C:\\Users\\natar_000\Pictures\Bing_Wallpapers\\",str(datetime.datetime.now().date())+'_bing.jpg')
+    p = os.path.join(os.path.expanduser('%USERPROFILE%'),'Pictures','Bing_Wallpapers')
+    file_name = str(datetime.datetime.now().date())+'_bing.jpg'
+    if not os.path.exists(p):
+        os.mkdir(p)
+    nam = p + '/' + file_name
     urlretrieve(path, nam)
     SPI_SETDESKWALLPAPER = 20
     ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, nam, 3)
+    
+def clean_old(p):
+    """Clean old images so that it doesn't occupy too much space.
+    At any point of time only 1 month old images will be kept"""
+    currentTime = time.time()
+    
+    for f in os.listdir(p):
+        full_path = os.path.join(p, f)
+        creation_time = os.path.getctime(full_path)
+        if os.path.isfile(full_path) and f.endswith('_bing.jpg'):
+            if ( currentTime - creation_time ) // ( 24 * 3600 ) > 30:
+                os.remove(full_path)
+        
 
 if __name__ == '__main__':
     base_url = "http://bingwallpaper.com"
